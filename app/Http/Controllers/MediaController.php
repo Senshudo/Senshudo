2cbdb208ead0597947fec6880814535e86b1efd8
Stewart Marsh <sweptsquash@live.co.uk>
2024-05-04 17:33:50 +0100

    change response for MediaController

--- app/Http/Controllers/MediaController.php
+++ app/Http/Controllers/MediaController.php
@@ -2,18 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Storage;
 use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
 class MediaController extends Controller
 {
-    public function __invoke(Media $media)
+    public function __invoke(Request $request, Media $media): Response
     {
         if (! App::isProduction() && ! Storage::disk('media')->exists($media->getPathRelativeToRoot())) {
             return response()->noContent();
         }
 
-        return response()->file($media->getPath());
+        return $media->toResponse($request);
     }
 }
